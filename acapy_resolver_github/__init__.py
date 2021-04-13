0167ba919275fddd301c3b3272292fbf729be8a1
Daniel Bluhm <dbluhm@pm.me>
2021-04-13 15:06:32 -0400

    feat: working resolver
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- acapy_resolver_github/__init__.py
+++ acapy_resolver_github/__init__.py
@@ -1,5 +1,13 @@
 """ACA-Py Resolver Github."""
 
+from aries_cloudagent.config.injection_context import InjectionContext
+from aries_cloudagent.resolver.did_resolver_registry import DIDResolverRegistry
 
-async def setup(context):
+from .resolver import GithubResolver
+
+
+async def setup(context: InjectionContext):
     """Setup the plugin."""
+    registry = context.inject(DIDResolverRegistry)
+    assert isinstance(registry, DIDResolverRegistry)
+    registry.register(GithubResolver())
